236940465161f9f6c9d315d9cf257d25fe1e271b
Benjamin Petetot <bpetetot@gmail.com>
2019-11-29 16:55:05 +0100

    :recycle: random path and color

--- RandomWalk/canvas.js
+++ RandomWalk/canvas.js
@@ -1,14 +1,11 @@
 let canvas;
 
-const root = document.getElementById('root')
-
 export function getCanvas() {
   return new Promise((resolve) => {
     if (canvas) resolve(canvas);
 
     SVG.on(document, 'DOMContentLoaded', () => {
-      canvas = SVG()
-        .addTo(root)
+      canvas = SVG('root')
         .size(300, 300);
 
       resolve(canvas);
---
+++ RandomWalk/dot.js
@@ -0,0 +1,22 @@
+export default class Dot {
+  constructor(x, y) {
+    this._x = x;
+    this._y = y;
+  }
+
+  get x() {
+    return this._x;
+  }
+
+  get y() {
+    return this._y;
+  }
+
+  set x(x) {
+    this._x = x;
+  }
+
+  set y(y) {
+    this._y = y;
+  }
+}
